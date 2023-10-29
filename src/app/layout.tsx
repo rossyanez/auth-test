5f40f0d444aac628d2200c7b1363d4ec3eff5a86
Ross Yanez <rusty.paoloyanez@gmail.com>
2023-10-30 07:58:23 +0800

    final update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="en">
 			<head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 				<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
 			</head>
 			<body>
